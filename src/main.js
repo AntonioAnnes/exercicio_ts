4aa58bac727cf5cefd97e27aab2ae6f03e76bc4a
Antonio Annes <antonio.vmachado13@gmail.com>
2023-05-26 20:39:41 -0300

    initial

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict";
+function Multiplica(numeroA, numeroB) {
+    numeroA = 5;
+    numeroB = 8;
+    let Resultado = numeroA * numeroB;
+}
+const Multiplica2 = (numeroA, numeroB) => numeroA * numeroB;
+// função 2
+function Saudacao(Olá, nome) {
+    return Olá + nome;
+}
+console.log(Saudacao);
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+function Multiplica(numeroA: number, numeroB: number) {
+    numeroA = 5
+    numeroB = 8
+    let Resultado = numeroA * numeroB
+}
+
+const Multiplica2 = (numeroA: number, numeroB: number): number => numeroA * numeroB
+
+// função 2
+
+function Saudacao(Olá: string, nome: string) {
+    return Olá + nome
+}
+
+console.log (Saudacao)
+
