4d7ab8309a7fb9d91be994d469662d3fecd4336c
Antonio Annes <antonio.vmachado13@gmail.com>
2023-05-29 11:54:00 -0300

    commit2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 "use strict";
 function Multiplica(numeroA, numeroB) {
-    numeroA = 5;
-    numeroB = 8;
     let Resultado = numeroA * numeroB;
+    return Resultado;
 }
 const Multiplica2 = (numeroA, numeroB) => numeroA * numeroB;
 // função 2
-function Saudacao(Olá, nome) {
-    return Olá + nome;
+function Saudacao(nome) {
+    return "Olá " + nome;
 }
-console.log(Saudacao);
+console.log(Saudacao("Jonas"));
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-function Multiplica(numeroA: number, numeroB: number) {
-    numeroA = 5
-    numeroB = 8
+function Multiplica(numeroA: number, numeroB: number): number {
     let Resultado = numeroA * numeroB
+    return Resultado
 }
+
+
+
 
 const Multiplica2 = (numeroA: number, numeroB: number): number => numeroA * numeroB
 
 // função 2
 
-function Saudacao(Olá: string, nome: string) {
-    return Olá + nome
-}
+function Saudacao(nome: string): string {
+    return "Olá " + nome  
+}  
 
-console.log (Saudacao)
+console.log(Saudacao("Jonas"))  
 
